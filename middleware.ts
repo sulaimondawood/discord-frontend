6e0e03632b9ee393b235c730135d0768fdf43180
Dawood <sulaimondawod@gmail.com>
2024-01-02 22:36:51 +0100

    new comit

--- middleware.ts
+++ middleware.ts
@@ -18,33 +18,33 @@
 //   matcher: ["/rooms/:path*", "/friends", "/account/:path*"],
 // };
 
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { isTokeneExpired } from "./utils/token-expired";
-import { jwtDecode } from "jwt-decode";
-import { clientBaseUrl } from "./utils/axios";
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get("refresh_token");
-  console.log("middleware");
-  console.log(token);
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { isTokeneExpired } from "./utils/token-expired";
+// import { jwtDecode } from "jwt-decode";
+// import { clientBaseUrl } from "./utils/axios";
+// export function middleware(req: NextRequest) {
+//   const token = req.cookies.get("refresh_token");
+//   console.log("middleware");
+//   console.log(token);
 
-  if (!token) {
-    return NextResponse.redirect(clientBaseUrl);
-  }
-  try {
-    const decodedToken = jwtDecode(token.value);
-    console.log(decodedToken);
-    if (isTokeneExpired(decodedToken)) {
-      return NextResponse.redirect(clientBaseUrl);
-    }
-  } catch (error) {
-    console.log(error);
-    return NextResponse.redirect(clientBaseUrl);
-  }
+//   if (!token) {
+//     return NextResponse.redirect(clientBaseUrl);
+//   }
+//   try {
+//     const decodedToken = jwtDecode(token.value);
+//     console.log(decodedToken);
+//     if (isTokeneExpired(decodedToken)) {
+//       return NextResponse.redirect(clientBaseUrl);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.redirect(clientBaseUrl);
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/rooms/:path*", "/friends", "/account/:path*"],
-};
+// export const config = {
+//   matcher: ["/rooms/:path*", "/friends", "/account/:path*"],
+// };
